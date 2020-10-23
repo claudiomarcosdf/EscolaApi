b8ab37e2e05429c718e5370adaf31f44694f799d
claudiomarcosdf <claudio.marcos.df@gmail.com>
2020-10-23 18:19:20 -0300

    Security includes

---
+++ src/api/user/user.js
@@ -0,0 +1,11 @@
+const restful = require('node-restful');
+const mongoose = restful.mongoose;
+
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  lastName: { type: String, required: true},
+  email: { type: String, required: true },
+  password: { type: String, required: true, min: 6, max: 12 },
+});
+
+module.exports = restful.model('user', userSchema);
--- src/config/routes.js
+++ src/config/routes.js
@@ -1,20 +1,31 @@
 const express = require('express');
+const auth = require('./auth');
 const AlunoRouter = require('../api/service/alunoService');
 const TabelaComportamentoRouter = require('../api/service/tabelaComportamentoService');
 const TabelaMedidaRouter = require('../api/service/tabelaMedidaService');
 const TabelaTransgressaoRouter = require('../api/service/tabelaTransgressaoService');
 
 module.exports = function (server) {
-  //URL Base
-  const router = express.Router();
-  server.use('/api', router);
+  /* Rotas protegidas por Token JWT */
+  const protectedApi = express.Router();
+  server.use('/api', protectedApi);
+  protectedApi.use(auth);
 
   //Rotas do Aluno
-  AlunoRouter.register(router, '/alunos');
+  AlunoRouter.register(protectedApi, '/alunos');
   //Rotas Tabela de Comportamento
-  TabelaComportamentoRouter.register(router, '/tabela-comportamento');
+  TabelaComportamentoRouter.register(protectedApi, '/tabela-comportamento');
   //Rotas Tabela de Medida
-  TabelaMedidaRouter.register(router, '/tabela-medida');
+  TabelaMedidaRouter.register(protectedApi, '/tabela-medida');
   //Rotas Tabela de Transgressao
-  TabelaTransgressaoRouter.register(router, '/tabela-transgressao');
+  TabelaTransgressaoRouter.register(protectedApi, '/tabela-transgressao');
+
+  /* Rotas abertas */
+  const openApi = express.Router();
+  server.use('/oapi', openApi);
+
+  const AuthService = require('../api/service/authService');
+  openApi.post('/login', AuthService.login);
+  openApi.post('/signup', AuthService.signup);
+  openApi.post('/validateToken', AuthService.validateToken);
 };
